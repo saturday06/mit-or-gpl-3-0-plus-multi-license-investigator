ea70d0234f44eb8551eedfce795739527eca0130
saturday06 <isamu@leafytree.jp>
2024-08-24 20:54:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MIT OR GPL-3.0-or-later Multi License Investigator
+
+OSSのライセンスを `SPDX-License-Identifier: MIT OR GPL-3.0-or-later` にしたい。どのようにレポジトリを構成すれば内容が正しくなり、また既存ライセンス処理ツールが読める状態にする調査のためのレポジトリ。
---
+++ src/mit_or_gpl_3_0_plus_multi_license_investigator/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for scaling-funicular."""
+
+
+def hello() -> str:
+    """Return a friendly greeting."""
+    return "Hello from scaling-funicular!"
+
+
+if __name__ == "__main__":
+    print(hello())
